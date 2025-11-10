06ae942bbdb1d20530fe436425f6ff3b21e7d855
Andrii Bondarenko <example651@gmail.com>
2025-11-10 21:37:25 +0200

    Rename getNotes to getAllNotes and update response format; remove test error route

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -11,9 +11,6 @@ const router = Router();
 
 router.get('/notes', getNotes);
 router.get('/notes/:noteId', getNoteById);
-router.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
 router.post('/notes', createNote);
 router.delete('/notes/:noteId', deleteNote);
 router.patch('/notes/:noteId', updateNote);
