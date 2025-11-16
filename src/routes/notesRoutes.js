7247c9f2d9959a6cb9fd66f50e4ef2eb01c08fbc
Andrii Bondarenko <example651@gmail.com>
2025-11-16 19:14:09 +0200

    main work commit

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -9,15 +9,17 @@ import {
 import { celebrate } from 'celebrate';
 import {
   createNoteSchema,
+  getAllNotesSchema,
   noteIdSchema,
+  updateNoteSchema,
 } from '../validations/notesValidation.js';
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,23 +1,15 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
-// export const getAllNotesSchema = Joi.object({
-//   page: Joi.number().integer().min(1).default(1),
-//   perPage: Joi.number().integer().min(5).max(20).default(10),
-//   tag: Joi.string().valid(
-//     'Work',
-//     'Personal',
-//     'Meeting',
-//     'Shopping',
-//     'Ideas',
-//     'Travel',
-//     'Finance',
-//     'Health',
-//     'Important',
-//     'Todo',
-//   ),
-//   search: Joi.string().allow(''),
-// });
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().trim().allow(''),
+  }),
+};
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
@@ -31,18 +23,7 @@ export const createNoteSchema = {
       'string.base': 'Content must be a string',
     }),
     tag: Joi.string()
-      .valid(
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      )
+      .valid(...TAGS)
       .messages({
         'string.base': 'Tag must be a string',
         'any.only':
@@ -58,4 +39,13 @@ export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
+};
+
+export const updateNoteSchema = {
+  ...noteIdSchema,
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
 };
